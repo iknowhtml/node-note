5120d70c80af6ed94603d0fa92d363f9c28fcddb
Aki Gao <xag9bb@virginia.edu>
2017-05-24 21:54:01 -0400

    complete 12. restarting app with nodemon

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash')
 
 const notes = require('./notes');
-
-let filteredArray = _.uniq(['Aki', 1, 'Aki', 1, 2, 3, 4]);
-console.log(filteredArray);
- 
-// console.log(_.isString(true));
-// console.log(_.isString('Aki'));
-
-// let res = notes.addNote();
-// console.log(res);
-//
-// console.log(`Result ${notes.add(9, -2)}`);
-
-// let user = os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
