653e47e828d956154f0eae87d7fc1a8d11e12dc1
Aki Gao <xag9bb@virginia.edu>
2017-05-24 22:02:52 -0400

    complete 13. getting input from user

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ const fs = require('fs');
 const _ = require('lodash')
 
 const notes = require('./notes');
+
+let command = process.argv[2];
+
+switch (command) {
+  case 'add':
+    console.log('Adding new note.');
+    break;
+
+  case 'list':
+    console.log('Listing all notes.');
+    break;
+
+  case 'read':
+    console.log('Reading note.');
+
+  case 'remove':
+    console.log('Removing note.');
+    break;
+
+  default:
+    console.log('Command not recognized.');
+}
